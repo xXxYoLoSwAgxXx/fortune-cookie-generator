82eb8de82dda90fd31b36749069f4e0d5fb0fe97
xXxYoLoSwAgxXx <harrisfambam@gmail.com>
2017-01-11 09:21:22 -1000

    stuff

---
+++ js/app.js
@@ -0,0 +1,20 @@
+/*intial variable named node so it can be the same name when i "append" it
+*/
+
+/*getting the element from the html
+*/
+
+/*making an array of my fortunes 
+*/
+var Array = ["you will live a long and healthy life", "you will die", "you will click another fortune", "you will not click any more fortunes", "quit life"];
+
+/*appending text to list
+
+node.appendChild(Array);*/
+// document.getElementById("fortune-cookie-text").appendChild()
+ function generateFortuneCookie() {
+   console.log("hello");
+   for (var i = 0; i >= Array.length; i++) {
+   Array[i]
+    }
+}
